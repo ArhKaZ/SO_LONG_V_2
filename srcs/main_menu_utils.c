a722fc8bedb82d025208f1d532cfba7246049ad4
Your Name <you@example.com>
2023-02-24 16:44:38 +0100

    try to fix close menu

--- srcs/main_menu_utils.c
+++ srcs/main_menu_utils.c
@@ -6,7 +6,7 @@
 /*   By: syluiset <syluiset@student42.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/24 02:10:20 by syluiset          #+#    #+#             */
-/*   Updated: 2023/02/24 16:42:36 by syluiset         ###   ########.fr       */
+/*   Updated: 2023/02/24 16:44:27 by syluiset         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@ void	free_menu(t_menu *menu)
 {
 	free_texture(menu->menu[0], menu->mlx->mlx);
 	free_texture(menu->menu[1], menu->mlx->mlx);
-	mlx_destroy_display(menu->mlx->mlx);
 	free(menu->mlx->mlx);
 	free(menu->mlx);
 	free(menu);
@@ -35,6 +34,7 @@ int	close_menu_exit(t_param *param)
 int	close_menu_start(t_param *param)
 {
 	mlx_destroy_window(param->menu->mlx->mlx, param->menu->mlx->mlx_win);
+	mlx_destroy_display(param->menu->mlx->mlx);
 	so_long(param);
 	return (1);
 }
