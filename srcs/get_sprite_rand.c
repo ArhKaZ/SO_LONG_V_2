2da5d3054376095ba0e11a945b4e0e544a34abf6
Your Name <you@example.com>
2023-03-02 14:59:16 +0100

    comment for sprite_obstacle

--- srcs/get_sprite_rand.c
+++ srcs/get_sprite_rand.c
@@ -6,26 +6,26 @@
 /*   By: syluiset <syluiset@student42.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/01 14:35:28 by syluiset          #+#    #+#             */
-/*   Updated: 2023/03/02 13:42:25 by syluiset         ###   ########.fr       */
+/*   Updated: 2023/03/02 14:58:47 by syluiset         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/so_long.h"
 
-void	*get_sprite_obstacle(t_sprite_planet *texts)
-{
-	void	*sprite;
-	int		random;
+// void	*get_sprite_obstacle(t_sprite_planet *texts)
+// {
+// 	void	*sprite;
+// 	int		random;
 
-	sprite = NULL;
-	random = get_random(4);
-	if (random == 0)
-		sprite = texts->planet_1->p;
-	if (random == 1)
-		sprite = texts->planet_2->p;
-	if (random == 2)
-		sprite = texts->planet_3->p;
-	if (random == 3)
-		sprite = texts->planet_4->p;
-	return (sprite);
-}
+// 	sprite = NULL;
+// 	random = get_random(4);
+// 	if (random == 0)
+// 		sprite = texts->planet_1->p;
+// 	if (random == 1)
+// 		sprite = texts->planet_2->p;
+// 	if (random == 2)
+// 		sprite = texts->planet_3->p;
+// 	if (random == 3)
+// 		sprite = texts->planet_4->p;
+// 	return (sprite);
+// }
