7f44aa56b4bc65cca042b17cf31cfe0b76b1b4d3
Your Name <you@example.com>
2023-03-02 15:03:22 +0100

    comment for sprite_obstacle

--- test.c
+++ test.c
@@ -1,4 +1,25 @@
+int	get_random(int nb_max)
+{
+	int	random_number;
+	int	fd;
+	int	ret;
+
+	fd = open("/dev/random", O_RDONLY);
+	if (fd < 0 || read(fd, 0, 0) < 0)
+		exit(EXIT_FAILURE);
+	ret = read(fd, &random_number, sizeof(int));
+	if (ret < 0)
+		return (-1);
+	close(fd);
+	random_number %= nb_max;
+	if (random_number < 0)
+	{
+		random_number = random_number * -1;
+	}
+	return (random_number);
+}
+#include <stdio.h>
 int main()
 {
-	ft_printf("%d/%d/%d/%d/%d", get_random(4), get_random(4), get_random(4), get_random(4), get_random(4));
+	printf("%d/%d/%d/%d/%d", get_random(4), get_random(4), get_random(4), get_random(4), get_random(4));
 }
