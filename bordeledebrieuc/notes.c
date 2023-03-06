a7f4b3b8de6e8aa07e030b9b1e5eeffe177bda74
arhkaz <sylvain.arhkaz@gmail.com>
2023-03-06 17:16:30 +0100

    bordelbrieux

---
+++ bordeledebrieuc/notes.c
@@ -0,0 +1,16 @@
+tu t'arretes au premier point pour les extensions.
+
+tu ouvres ton file sans checker si tu as reussi get_map to_stringo
+all check map ton ft_split est pas securo
+
+verif ship (je n'ai pas compris mais je note
+
+le cp map ligne 42 test_map.c est pas securise SEG FAULT :) 
+
+ton free dans free_char_map ligne 27 free toujours NULL donc c'est pas ultra utile.
+
+create_empty mlx dans main menu -> securisation
+
+mlx->init meme fichier ->securisation 
+new window aussi
+icreate t_menu
